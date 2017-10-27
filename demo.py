35d3ed6cdb0ffae11844b71cac7f9607a9350039
crazyalan <cxc409099887@gmail.com>
2017-10-27 12:17:58 -0700

    modified opencv function

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 from utils import *
 from darknet import Darknet
 import cv2
+import imageio
+import numpy as np
 
 def demo(cfgfile, weightfile):
     m = Darknet(cfgfile)
@@ -20,20 +22,31 @@ def demo(cfgfile, weightfile):
     if use_cuda:
         m.cuda()
 
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Unable to open camera")
-        exit(-1)
+    # cap = cv2.VideoCapture(0)
+    reader = imageio.get_reader('<video0>')
+
+    # if not cap.isOpened():
+    #     print("Unable to open camera")
+    #     exit(-1)
 
     while True:
-        res, img = cap.read()
+        # import pdb
+        # pdb.set_trace()
+        # res, img = cap.read()
+        img = reader.get_next_data()
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        res = True
         if res:
             sized = cv2.resize(img, (m.width, m.height))
             bboxes = do_detect(m, sized, 0.5, 0.4, use_cuda)
             print('------')
             draw_img = plot_boxes_cv2(img, bboxes, None, class_names)
-            cv2.imshow(cfgfile, draw_img)
-            cv2.waitKey(1)
+            # import pdb
+            # pdb.set_trace()
+            # print np.shape(draw_img)
+            cv2.imshow('cfgfile', img)
+            cv2.waitKey(50)
         else:
              print("Unable to read image")
              exit(-1) 
